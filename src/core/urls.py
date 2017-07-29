e18b9508a43cf6678e95cf3559378067991aadd7
Javier Sanchez Toledano <js.toledano@me.comx>
2017-07-29 10:06:26 -0500

    Se corrigen los namespaces de blog

--- src/core/urls.py
+++ src/core/urls.py
@@ -2,15 +2,15 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 
-handler400 = 'blog.views.error500'
-handler404 = 'blog.views.error404'
-handler500 = 'blog.views.error500'
+handler400 = 'apps.blog.views.error500'
+handler404 = 'apps.blog.views.error404'
+handler500 = 'apps.blog.views.error500'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('authtools.urls')),
     url(r'^admin_tools/', include('admin_tools.urls')),
-    url(r'', include('blog.uris')),
+    url(r'', include('apps.blog.uris')),
 ]
 
 if settings.DEBUG:
