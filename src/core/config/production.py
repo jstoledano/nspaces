5a88364ff8be14e12a66e49fae71056d54552fcf
Javier Sanchez Toledano <js.toledano@me.com>
2017-07-29 09:55:07 -0500

    Rollbar en producción

--- src/core/config/production.py
+++ src/core/config/production.py
@@ -5,3 +5,9 @@ MIDDLEWARE += [
     'rollbar.contrib.django.middleware.RollbarNotifierMiddleware'
 ]
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
+ROLLBAR = {
+    'access_token': env('ROLLBAR_TOKEN'),
+    'environment': 'production',
+    'branch': 'master',
+    'root': ROOT_DIR,
+}
