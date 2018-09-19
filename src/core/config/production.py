cf91fff517c0ef8e19e86c7078558c3596c898a2
Javier Sanchez Toledano <js.toledano@me.comx>
2018-09-19 08:45:38 -0500

    Google tag

--- src/core/config/production.py
+++ src/core/config/production.py
@@ -2,7 +2,7 @@ from .base import *    # noqa
 
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 MIDDLEWARE += [
-    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware'
+    # 'rollbar.contrib.django.middleware.RollbarNotifierMiddleware'
 ]
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 ROLLBAR = {
